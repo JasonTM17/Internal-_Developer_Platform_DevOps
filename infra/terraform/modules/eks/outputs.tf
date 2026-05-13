fd1ff04204cbda9465fd6f5af0c6bd891898eb75
Nguyen Son <jasonbmt06@gmail.com>
2026-05-13 11:21:04 +0700

    feat(terraform): add EKS module outputs

---
+++ infra/terraform/modules/eks/outputs.tf
@@ -0,0 +1,73 @@
+################################################################################
+# EKS Module Outputs
+################################################################################
+
+output "cluster_id" {
+  description = "ID/name of the EKS cluster"
+  value       = aws_eks_cluster.main.id
+}
+
+output "cluster_arn" {
+  description = "ARN of the EKS cluster"
+  value       = aws_eks_cluster.main.arn
+}
+
+output "cluster_endpoint" {
+  description = "Endpoint for the EKS cluster API server"
+  value       = aws_eks_cluster.main.endpoint
+}
+
+output "cluster_certificate_authority_data" {
+  description = "Base64 encoded certificate data for cluster authentication"
+  value       = aws_eks_cluster.main.certificate_authority[0].data
+}
+
+output "cluster_version" {
+  description = "Kubernetes version of the EKS cluster"
+  value       = aws_eks_cluster.main.version
+}
+
+output "cluster_security_group_id" {
+  description = "Security group ID attached to the EKS cluster"
+  value       = aws_security_group.cluster.id
+}
+
+output "node_security_group_id" {
+  description = "Security group ID attached to the EKS worker nodes"
+  value       = aws_security_group.node.id
+}
+
+output "oidc_provider" {
+  description = "OIDC provider for the EKS cluster (without https://)"
+  value       = replace(aws_eks_cluster.main.identity[0].oidc[0].issuer, "https://", "")
+}
+
+output "oidc_provider_arn" {
+  description = "ARN of the OIDC provider"
+  value       = aws_iam_openid_connect_provider.eks.arn
+}
+
+output "oidc_issuer" {
+  description = "OIDC issuer URL for the EKS cluster"
+  value       = aws_eks_cluster.main.identity[0].oidc[0].issuer
+}
+
+output "cluster_role_arn" {
+  description = "ARN of the EKS cluster IAM role"
+  value       = aws_iam_role.cluster.arn
+}
+
+output "node_role_arn" {
+  description = "ARN of the EKS node group IAM role"
+  value       = aws_iam_role.node.arn
+}
+
+output "kms_key_arn" {
+  description = "ARN of the KMS key used for EKS secrets encryption"
+  value       = aws_kms_key.eks.arn
+}
+
+output "kms_key_id" {
+  description = "ID of the KMS key used for EKS secrets encryption"
+  value       = aws_kms_key.eks.key_id
+}
