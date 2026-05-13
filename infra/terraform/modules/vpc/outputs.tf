1d12cb68d56d958cc9ac30636d15ea5e8acd9f77
Nguyen Son <jasonbmt06@gmail.com>
2026-05-13 11:18:55 +0700

    feat(terraform): add VPC module outputs

---
+++ infra/terraform/modules/vpc/outputs.tf
@@ -0,0 +1,68 @@
+################################################################################
+# VPC Module Outputs
+################################################################################
+
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = aws_vpc.main.id
+}
+
+output "vpc_cidr_block" {
+  description = "CIDR block of the VPC"
+  value       = aws_vpc.main.cidr_block
+}
+
+output "vpc_arn" {
+  description = "ARN of the VPC"
+  value       = aws_vpc.main.arn
+}
+
+output "public_subnet_ids" {
+  description = "IDs of the public subnets"
+  value       = aws_subnet.public[*].id
+}
+
+output "public_subnet_cidrs" {
+  description = "CIDR blocks of the public subnets"
+  value       = aws_subnet.public[*].cidr_block
+}
+
+output "private_subnet_ids" {
+  description = "IDs of the private subnets"
+  value       = aws_subnet.private[*].id
+}
+
+output "private_subnet_cidrs" {
+  description = "CIDR blocks of the private subnets"
+  value       = aws_subnet.private[*].cidr_block
+}
+
+output "database_subnet_ids" {
+  description = "IDs of the database subnets"
+  value       = aws_subnet.database[*].id
+}
+
+output "database_subnet_cidrs" {
+  description = "CIDR blocks of the database subnets"
+  value       = aws_subnet.database[*].cidr_block
+}
+
+output "internet_gateway_id" {
+  description = "ID of the Internet Gateway"
+  value       = aws_internet_gateway.main.id
+}
+
+output "nat_gateway_ids" {
+  description = "IDs of the NAT Gateways"
+  value       = aws_nat_gateway.main[*].id
+}
+
+output "availability_zones" {
+  description = "Availability zones used by the VPC"
+  value       = var.availability_zones
+}
+
+output "flow_log_group_name" {
+  description = "Name of the CloudWatch log group for VPC flow logs"
+  value       = aws_cloudwatch_log_group.flow_log.name
+}
